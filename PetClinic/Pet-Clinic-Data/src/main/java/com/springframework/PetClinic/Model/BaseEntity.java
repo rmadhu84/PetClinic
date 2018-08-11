eb416747cd6690cd1b8c60eae53d21980d6378a9
rmadhu84 <r.madhu.84@gmail.com>
2018-08-11 17:49:06 -0400

    Added Base entity class

---
+++ PetClinic/Pet-Clinic-Data/src/main/java/com/springframework/PetClinic/Model/BaseEntity.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.springframework.PetClinic.Model;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Madhu
+ *
+ */
+@Getter
+@Setter
+public class BaseEntity implements Serializable {
+	
+	private static final long serialVersionUID = -1957837592305842857L;
+	private Long id;
+}
