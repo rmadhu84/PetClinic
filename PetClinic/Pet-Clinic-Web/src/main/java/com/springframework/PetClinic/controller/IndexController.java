f39e52556de783b173eb24db9d0b0c68dfa8bcd9
ramachandranm1 <ramachandranm1@T60844XZWY52.som.ad.state.mi.us>
2018-08-14 12:57:57 -0400

    Added Pet Clinic Index page and controllers. Closes #9

---
+++ PetClinic/Pet-Clinic-Web/src/main/java/com/springframework/PetClinic/controller/IndexController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.springframework.PetClinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author ramachandranm1
+ *
+ */
+
+@Controller
+public class IndexController {
+
+	@RequestMapping({"", "/", "index", "index.html"})
+	public String index(Model model) {
+		return "index";
+	}
+}
