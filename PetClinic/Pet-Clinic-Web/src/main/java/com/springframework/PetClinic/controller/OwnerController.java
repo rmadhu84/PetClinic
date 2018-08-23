8f7fb7ccabbe2e1a7db0fea8ec39bac3fc7eba47
rmadhu84 <r.madhu.84@gmail.com>
2018-08-23 07:10:58 -0400

    Listed owners on Index page. closes#14

--- PetClinic/Pet-Clinic-Web/src/main/java/com/springframework/PetClinic/controller/OwnerController.java
+++ PetClinic/Pet-Clinic-Web/src/main/java/com/springframework/PetClinic/controller/OwnerController.java
@@ -3,8 +3,13 @@
  */
 package com.springframework.PetClinic.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springframework.PetClinic.Model.Owner;
+import com.springframework.PetClinic.services.OwnerService;
 
 /**
  * @author ramachandranm1
@@ -14,8 +19,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+	private OwnerService ownerService;
+	
+	
+	/**
+	 * @param ownerService
+	 */
+	@Autowired
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
+
 	@RequestMapping({"","/", "/index", "/index.html"})
-	public String listOwners() {
+	public String listOwners(Model model) {
+		for(Owner owner:ownerService.findAll()) {
+		System.out.println(owner.getFirstName()+" "+owner.getLastName());
+		}
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
 }
