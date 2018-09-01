b74eacc3da7fced620edca14070d0e8f84cdf33d
rmadhu84 <r.madhu.84@gmail.com>
2018-09-01 11:34:03 -0400

    Added addtional properties to Owner Class. Closes #26

--- PetClinic/Pet-Clinic-Data/src/main/java/com/springframework/PetClinic/Model/Owner.java
+++ PetClinic/Pet-Clinic-Data/src/main/java/com/springframework/PetClinic/Model/Owner.java
@@ -23,4 +23,8 @@ public class Owner extends Person {
 	 */
 	private static final long serialVersionUID = -7684297130966446439L;
 	private Set<Pet> pets = new HashSet<Pet>();
+	
+	private String address;
+	private String city;
+	private String telephone;
 }
