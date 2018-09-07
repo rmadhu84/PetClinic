0ea651c6295ef3f002e8fdda39f89eeadc080087
rmadhu84 <r.madhu.84@gmail.com>
2018-09-06 20:19:43 -0400

    Created visit entity jpa mapping. closes #36

--- PetClinic/Pet-Clinic-Data/src/main/java/com/springframework/PetClinic/Model/Pet.java
+++ PetClinic/Pet-Clinic-Data/src/main/java/com/springframework/PetClinic/Model/Pet.java
@@ -4,11 +4,15 @@
 package com.springframework.PetClinic.Model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.security.auth.callback.LanguageCallback;
 
@@ -39,4 +43,7 @@ public class Pet extends BaseEntity{
 	@ManyToOne
 	@JoinColumn(name = "owner_id")
 	private Owner owner;
+	
+	@OneToMany(cascade=CascadeType.ALL, mappedBy="pet")
+	private Set<Visit> visits = new HashSet<Visit>();
 }
--- PetClinic/Pet-Clinic-Data/src/main/java/com/springframework/PetClinic/Model/Visit.java
+++ PetClinic/Pet-Clinic-Data/src/main/java/com/springframework/PetClinic/Model/Visit.java
@@ -2,18 +2,33 @@ package com.springframework.PetClinic.Model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name="visits")
 public class Visit extends BaseEntity {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -7510853318612568878L;
+	
+	@Column(name="date")
 	private LocalDate date;
+	
+	@Column(name="description")
 	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name="pet_id")
 	private Pet Pet;
 }
